9e14fea33262716816561c3e9bec21a9b5629572
rika-tula <ricazabala04@gamil.com>
2024-01-05 20:40:49 -0300

    hice parte del html

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const path = require('path');
 
 const app = express();
 const port = 3030;
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=> {
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+app.listen(port)
