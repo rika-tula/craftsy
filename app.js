ae80d2640c1b7b4156e307815521b680c545a618
rika-tula <ricazabala04@gamil.com>
2024-01-09 16:44:41 -0300

    hice un poquito de css

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.get('/',(req,res)=> {
     res.sendFile(path.join(__dirname,'./views/home.html'))
 })
 
-app.listen(port, () => console.log('abierto el servidor'))
+app.listen(port, () => console.log('el servidor esta abierto'))
